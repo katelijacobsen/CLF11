c7b4a8ac7bacff1b5043fdc6b8a4991d46714336
Katja Mähleke <katjamaehleke98@gmail.com>
2024-03-21 15:18:22 +0100

    remove /

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://katelijacobsen.github.io',
-    base: '/CLF11', 
+    base: 'CLF11',
+    // site: 'https://katelijacobsen.github.io',
+    // base: '/CLF11',
 
 });
