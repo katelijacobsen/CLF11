ea718cae56122687ba64baf3035e7f316b18ab14
Katja Mähleke <katjamaehleke98@gmail.com>
2024-03-21 15:33:54 +0100

    turn off the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://katelijacobsen.github.io',
-    base: '/CLF11',
+   /* site: 'https://katelijacobsen.github.io',
+    base: '/CLF11',*/
     // site: 'https://katelijacobsen.github.io',
     // base: '/CLF11',
 
